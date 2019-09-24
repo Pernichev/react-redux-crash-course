3e0ae7b1fccc54fd2d225ae3f5972d76d86be271
mpernichev <pernichevm@gmail.com>
2019-09-24 23:36:03 +0300

    added post adding

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -9,9 +9,14 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_POSTS:
       return {
-          ...state,
-          items: action.payload
-      }
+        ...state,
+        items: action.payload
+      };
+    case NEW_POST:
+      return {
+        ...state,
+        item: action.payload
+      };
     default:
       return state;
   }
